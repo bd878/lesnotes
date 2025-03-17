8a950ed8b1210213e0cae68ea983a9a8a5acc19e
bd878 <slavad878@gmail.com>
2025-03-17 07:40:28 +0300

    newThread; is_private migration

--- server/schema/files.sql
+++ server/schema/files.sql
@@ -1,8 +1,8 @@
 PRAGMA user_version=1;
-PRAGMA foreigh_keys=ON;
+PRAGMA foreign_keys=ON;
 CREATE TABLE IF NOT EXISTS files(
-  id                 INTEGER UNIQUE NOT NULL,
-  user_id            INTEGER NOT NULL,
-  name               TEXT NOT NULL,
-  create_utc_nano    INTEGER NOT NULL
+	id                 INTEGER UNIQUE NOT NULL,
+	user_id            INTEGER NOT NULL,
+	name               TEXT NOT NULL,
+	create_utc_nano    INTEGER NOT NULL
 );
