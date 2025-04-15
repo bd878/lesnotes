c34d941be21eba6b234d2b2f8e5501d08f618745
bd878 <slavad878@gmail.com>
2025-04-15 11:48:27 +0300

    send/read public message

---
+++ server/scripts/read_public_message.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/bash
+
+# Reads a public message
+
+stage=${STAGE:-"stage."}
+cookie=${COOKIE:-""}
+id=${1:?"Usage: ./read_message.sh message_id"}
+
+cmd=`cat <<HERE
+sed -e "s/%STAGE%/$stage/g" \
+-e "s/%ID%/$id/g" ./curl/read_public_message.curl |
+curl -b $cookie -v -K -
+HERE`
+result=`eval "$cmd"`
+echo $result
