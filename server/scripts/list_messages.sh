0496f84a025f07d35b2665aadc3d980ab563a8c7
bd878 <slavad878@gmail.com>
2025-04-16 11:55:29 +0300

    list_/thread_messages both private/public

--- server/scripts/list_messages.sh
+++ server/scripts/list_messages.sh
@@ -4,12 +4,22 @@
 
 stage=${STAGE:-"stage."}
 cookie=${COOKIE:-"cookie.txt"}
-limit=${1?"Usage: list_messages.sh limit offset"}
-offset=${2?"Usage: list_messages.sh limit offset"}
+limit=${1?"Usage: list_messages.sh limit offset public=-1"}
+offset=${2?"Usage: list_messages.sh limit offset public=-1"}
+public=${3:-"-1"}
+
+public_filter=""
+
+if [[ "$public" == "1" ]]; then
+	public_filter="public=1"
+elif [[ "$public" == "0" ]]; then
+	public_filter="public=0"
+fi
 
 cmd=`cat <<HERE
 sed -e "s/%STAGE%/$stage/g" \
 -e "s/%LIMIT%/$limit/g" \
+-e "s/%PUBLIC%/$public_filter/g" \
 -e "s/%OFFSET%/$offset/g" ./curl/list_messages.curl |
 curl -b $cookie -s -K -
 HERE`
--- server/scripts/list_thread_messages.sh
+++ server/scripts/list_thread_messages.sh
@@ -4,14 +4,24 @@
 
 stage=${STAGE:-"stage."}
 cookie=${COOKIE:-"cookie.txt"}
-limit=${LIMIT:-"20"}
-offset=${OFFSET:-"0"}
-thread_id=${1?"Usage: list_thread_message.sh thread_id"}
+thread_id=${1?"Usage: list_thread_message.sh thread_id limit offset public=-1"}
+limit=${2?"Usage: list_thread_messages.sh thread_id limit offset public=-1"}
+offset=${3?"Usage: list_thread_messages.sh thread_id limit offset public=-1"}
+public=${4:-"-1"}
+
+public_filter=""
+
+if [[ "$public" == "1" ]]; then
+	public_filter="public=1"
+elif [[ "$public" == "0" ]]; then
+	public_filter="public=0"
+fi
 
 cmd=`cat <<HERE
 sed -e "s/%STAGE%/$stage/g" \
 -e "s/%LIMIT%/$limit/g" \
 -e "s/%THREAD_ID%/$thread_id/g" \
+-e "s/%PUBLIC%/$public_filter/g" \
 -e "s/%OFFSET%/$offset/g" ./curl/list_thread_messages.curl |
 curl -b $cookie -s -K -
 HERE`
