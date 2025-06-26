5b48ca75a371f8647567c773198fcfeb5b7dd18a
bd878 <slavad878@gmail.com>
2025-06-26 14:50:13 +0300

    tag pass style

--- front/client/gui/components/Tag/Tag.tsx
+++ front/client/gui/components/Tag/Tag.tsx
@@ -7,6 +7,7 @@ const Tag = forwardRef((props, ref) => {
 		<TagName
 			ref={ref}
 			id={props.id}
+			style={props.style}
 			tabIndex={props.tabIndex}
 			htmlFor={props.htmlFor}
 			disabled={props.disabled}
