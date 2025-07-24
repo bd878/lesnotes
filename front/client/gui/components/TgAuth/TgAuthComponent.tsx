a07741b188d6e44d6ee9ff47daad946323217f61
bd878 <slavad878@gmail.com>
2025-07-24 14:29:57 +0300

    create element script

--- front/client/gui/components/TgAuth/TgAuthComponent.tsx
+++ front/client/gui/components/TgAuth/TgAuthComponent.tsx
@@ -6,16 +6,16 @@ function TgAuthComponent() {
 	useEffect(() => {
 		if (ref.current == null)
 			return
-
-		const scriptEl = document.createElement("script", {
-			"async": true,
-			"src": "https://telegram.org/js/telegram-widget.js?22",
-			"data-telegram-login": `${BOT_USERNAME}`,
-			"data-size": "small",
-			"data-auth-url": `https://${BACKEND_URL}/tg_auth"`,
-			"data-request-access": "write",
-		});
-		ref.current.append(scriptEl)
+		if (!ref.current.hasChildNodes()) {
+			const scriptEl = document.createElement("script");
+			scriptEl.setAttribute("async", true)
+			scriptEl.setAttribute("src", "https://telegram.org/js/telegram-widget.js?22")
+			scriptEl.setAttribute("data-telegram-login", `${BOT_USERNAME}`)
+			scriptEl.setAttribute("data-size", "small")
+			scriptEl.setAttribute("data-auth-url", `https://${BACKEND_URL}/tg_auth"`)
+			scriptEl.setAttribute("data-request-access", "write")
+			ref.current.appendChild(scriptEl)
+		}
 	}, [ref])
 
 	return (
