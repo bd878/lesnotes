0d9f996f96bf7f17f21fdded49f56c42e012e0a6
bd878 <slavad878@gmail.com>
2025-07-24 14:24:34 +0300

    append script

--- front/client/gui/components/TgAuth/TgAuthComponent.tsx
+++ front/client/gui/components/TgAuth/TgAuthComponent.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react'
 
 function TgAuthComponent() {
+	const ref = useRef(null)
+
+	useEffect(() => {
+		if (ref.current == null)
+			return
+
+		const scriptEl = document.createElement("script", {
+			"async": true,
+			"src": "https://telegram.org/js/telegram-widget.js?22",
+			"data-telegram-login": `${BOT_USERNAME}`,
+			"data-size": "small",
+			"data-auth-url": `https://${BACKEND_URL}/tg_auth"`,
+			"data-request-access": "write",
+		});
+		ref.current.append(scriptEl)
+	}, [ref])
+
 	return (
-		<script async onLoad={() => {}} src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login={`${BOT_USERNAME}`} data-size="small" data-auth-url={`https://${BACKEND_URL}/tg_auth"`} data-request-access="write"></script>
+		<div id="telegram-login-widget" ref={ref}>
+		</div>
 	)
 }
 
