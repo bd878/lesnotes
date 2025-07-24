caba16e8019dfd77f57533290b5f0c3782368364
bd878 <slavad878@gmail.com>
2025-07-24 14:00:40 +0300

    async empty string

--- front/client/gui/components/TgAuth/TgAuthComponent.tsx
+++ front/client/gui/components/TgAuth/TgAuthComponent.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function TgAuthComponent() {
 	return (
-		<script src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login={`${BOT_USERNAME}`} data-size="small" data-auth-url={`https://${BACKEND_URL}/tg_auth"`} data-request-access="write"></script>
+		<script async="" src="https://telegram.org/js/telegram-widget.js?22" data-telegram-login={`${BOT_USERNAME}`} data-size="small" data-auth-url={`https://${BACKEND_URL}/tg_auth"`} data-request-access="write"></script>
 	)
 }
 
