5529fcf67740aae9b265b560ec22ce9ea622aefc
bd878 <slavad878@gmail.com>
2025-08-16 15:55:24 +0300

    init pg schema

---
+++ server/schema/02_messages.sql
@@ -0,0 +1,24 @@
+\c lesnotes
+CREATE SCHEMA IF NOT EXISTS messages;
+
+CREATE TABLE IF NOT EXISTS messages.messages
+(
+	id           bigint        UNIQUE NOT NULL,
+	text         TEXT          NOT NULL,
+	file_ids     jsonb         DEFAULT NULL,
+	private      bool          NOT NULL DEFAULT true,
+	name         VARCHAR(256)  UNIQUE NOT NULL,
+	user_id      bigint        NOT NULL,
+	thread_id    bigint        DEFAULT NULL,
+	created_at   timestamptz   NOT NULL DEFAULT NOW(),
+	updated_at   timestamptz   NOT NULL DEFAULT NOW(),
+	PRIMARY KEY(id)
+);
+
+CREATE INDEX IF NOT EXISTS messages_text ON messages.messages(text);
+
+CREATE TRIGGER created_at_messages_trgr BEFORE UPDATE ON messages.messages FOR EACH ROW EXECUTE PROCEDURE created_at_trigger();
+CREATE TRIGGER updated_at_messages_trgr BEFORE UPDATE ON messages.messages FOR EACH ROW EXECUTE PROCEDURE updated_at_trigger();
+
+GRANT USAGE ON SCHEMA messages TO lesnotes_admin;
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA messages TO lesnotes_admin;
---
+++ server/schema/05_sessions.sql
@@ -0,0 +1,16 @@
+\c lesnotes
+CREATE SCHEMA IF NOT EXISTS sessions;
+
+CREATE TABLE IF NOT EXISTS sessions.sessions
+(
+	user_id     bigint       NOT NULL,
+	value       VARCHAR(256) NOT NULL,
+	expires_at  timestamptz  NOT NULL,
+	created_at  timestamptz  NOT NULL DEFAULT NOW(),
+	PRIMARY KEY(id)
+);
+
+CREATE TRIGGER created_at_sessions_trgr BEFORE UPDATE ON sessions.sessions FOR EACH ROW EXECUTE PROCEDURE created_at_trigger();
+
+GRANT USAGE ON SCHEMA sessions TO lesnotes_admin;
+GRANT INSERT, UPDATE, DELETE, SELECT ON ALL TABLES IN SCHEMA sessions TO lesnotes_admin;
