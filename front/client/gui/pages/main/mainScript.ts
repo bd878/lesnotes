7fe54008d8eb61871c0b678c9d92e22b2ec8a3f1
bd878 <slavad878@gmail.com>
2025-08-30 17:42:47 +0300

    main script

--- front/client/gui/pages/main/mainScript.ts
+++ front/client/gui/pages/main/mainScript.ts
@@ -1,4 +1,17 @@
 function init() {
+	const scriptEl = document.createElement("script");
+	scriptEl.setAttribute("async", true)
+	scriptEl.setAttribute("src", "https://telegram.org/js/telegram-widget.js?22")
+	scriptEl.setAttribute("data-telegram-login", `${BOT_USERNAME}`)
+	scriptEl.setAttribute("data-size", "small")
+	scriptEl.setAttribute("data-auth-url", `https://${BACKEND_URL}/tg_auth`)
+	scriptEl.setAttribute("data-request-access", "write")
+	const widgetElem = document.getElementById("telegram-login-widget")
+	if (!widgetElem) {
+		console.error("[mainScript]: no widget element")
+		return
+	}
+	widgetElem.appendChild(scriptEl)
 }
 
 window.addEventListener("load", () => {
--- front/environment.d.ts
+++ front/environment.d.ts
@@ -2,6 +2,7 @@ declare global {
 	var ENV: string;
 	var HTTPS: string;
 	var BACKEND_URL: string;
+	var BOT_USERNAME: string;
 	var BOT_VALIDATE_AUTH_URL: string;
 	var BOT_VALIDATE_URL: string
 };
