954edaa254ef7185f6da9f926b75857a48860048
bd878 <slavad878@gmail.com>
2025-08-31 15:53:48 +0300

    fetch styles on server

--- front/client/routes/main.ts
+++ front/client/routes/main.ts
@@ -10,6 +10,7 @@ async function main(ctx) {
 
 	const resp = await api.authJson(token)
 	if (resp.error.error || resp.expired) {
+		const styles = await readFile(resolve(join(Config.get('basedir'), 'public/styles.css')), { encoding: 'utf-8' });
 		const layout = await readFile(resolve(join(Config.get('basedir'), 'templates/layout.mustache')), { encoding: 'utf-8' });
 		const main = await readFile(resolve(join(Config.get("basedir"), 'templates/main.mustache')), { encoding: 'utf-8' });
 		const footer = await readFile(resolve(join(Config.get("basedir"), 'templates/footer.mustache')), { encoding: 'utf-8' });
@@ -18,7 +19,7 @@ async function main(ctx) {
 			login:    i18n("login"),
 			register: i18n("register"),
 			scripts:  ["/public/mainScript.js"],
-			styles:   ["/public/styles.css"],
+			styles:   styles,
 		}, {
 			content:  main,
 			footer:   footer,
