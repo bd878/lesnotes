b20365e9d1f0a9550f0d7ac912dd635f76e37772
bd878 <slavad878@gmail.com>
2025-10-11 14:38:55 +0300

    connect search to db

--- server/search/internal/repository/postgres/postgres.go
+++ server/search/internal/repository/postgres/postgres.go
@@ -1,8 +1,11 @@
 package postgres
 
 import (
+	"os"
+	"fmt"
 	"context"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -17,10 +20,59 @@ func New(pool *pgxpool.Pool, messagesTableName, filesTableName string) *Reposito
 }
 
 func (r *Repository) SaveMessage(ctx context.Context, id, userID int64, name, title, text string) (err error) {
+	const query = "INSERT INTO %s(id, user_id, name, title, text) VALUES ($1, $2, $3, $4, $5)"
+
+	var tx pgx.Tx
+	tx, err = r.pool.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+	defer func() {
+		p := recover()
+		switch {
+		case p != nil:
+			_ = tx.Rollback(ctx)
+			panic(p)
+		case err != nil:
+			fmt.Fprintf(os.Stderr, "rollback with error: %v\n", err)
+			err = tx.Rollback(ctx)
+		default:
+			err = tx.Commit(ctx)
+		}
+	}()
+
+	_, err = tx.Exec(ctx, r.messagesTable(query), id, userID, name, title, text)
+
 	return nil
 }
 
 func (r *Repository) DeleteMessage(ctx context.Context, id, userID int64) (err error) {
+	const query = "DELETE FROM %s WHERE id = $1 AND user_id = $2"
+
+	var tx pgx.Tx
+	tx, err = r.pool.BeginTx(ctx, pgx.TxOptions{})
+	if err != nil {
+		return err
+	}
+	defer func() {
+		p := recover()
+		switch {
+		case p != nil:
+			_ = tx.Rollback(ctx)
+			panic(p)
+		case err != nil:
+			fmt.Fprintf(os.Stderr, "rollback with error: %v\n", err)
+			err = tx.Rollback(ctx)
+		default:
+			err = tx.Commit(ctx)
+		}
+	}()
+
+	_, err = tx.Exec(ctx, r.messagesTable(query), id, userID)
+
 	return nil
 }
 
+func (r Repository) messagesTable(query string) string {
+	return fmt.Sprintf(query, r.messagesTableName)
+}
