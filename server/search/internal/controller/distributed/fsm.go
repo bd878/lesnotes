11c7354fdb3cecced91e28cf45d27e3f11f7400d
bd878 <slavad878@gmail.com>
2026-02-18 11:36:28 +0300

    log

--- server/search/internal/controller/distributed/fsm.go
+++ server/search/internal/controller/distributed/fsm.go
@@ -67,6 +67,7 @@ type fsm struct {
 func (f *fsm) Apply(record *raft.Log) interface{} {
 	buf := record.Data
 	reqType := RequestType(buf[0])
+	logger.Debugw("apply fsm", "req_type", reqType)
 	switch reqType {
 	case AppendMessageRequest:
 		return f.applyAppendMessage(buf[1:])
