387f6f3800ccf2aace139f75bb18489cf82d391a
bd878 <slavad878@gmail.com>
2026-07-22 15:40:55 +0300

    change grpc timeout settings

--- server/internal/rpc/client.go
+++ server/internal/rpc/client.go
@@ -14,10 +14,11 @@ func NewClient(target string, opts ...grpc.DialOption) (conn *grpc.ClientConn, e
 			grpc.WithTransportCredentials(insecure.NewCredentials()),
 			grpc.WithStatsHandler(otelgrpc.NewClientHandler()),
 			grpc.WithKeepaliveParams(keepalive.ClientParameters{
-				Time: 5*time.Minute,
-				Timeout: 10*time.Second,
+				Time: 30*time.Second,
+				Timeout: 5*time.Second,
 				PermitWithoutStream: true,
 			}),
+			grpc.WithIdleTimeout(2 * time.Minute),
 		}, opts...)...,
 	)
 }
