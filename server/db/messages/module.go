0f59bf35d50a17a3870a96f2c8cae734a1bee23d
bd878 <slavad878@gmail.com>
2026-09-13 19:20:07 +0300

    serf_log_level

--- server/db/messages/module.go
+++ server/db/messages/module.go
@@ -56,6 +56,7 @@ func setupSerf(svc system.Service, cfg config.Config, handler serf.Handler) erro
 		serf.Config{
 			NodeName: cfg.NodeName,
 			BindAddr: cfg.SerfAddr,
+			LogLevel: cfg.SerfLogLevel,
 			Tags: map[string]string{
 				"raft_addr": cfg.RpcAddr,
 			},
